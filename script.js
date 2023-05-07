bfadd814fe855ca434418ac58a1cc91e170dfe28
kumarharsh13 <bigbangkumarharsh13@gmail.com>
2023-05-07 21:02:25 +0530

    Reset or New

--- script.js
+++ script.js
@@ -12,15 +12,29 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// Starting Condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting Condition
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -28,7 +42,6 @@ const switchPlayer = function () {
   currentScore = 0;
   player0El.classList.toggle("player--active");
   player1El.classList.toggle("player--active");
-  s;
 };
 
 // Rolling dice
@@ -64,7 +77,7 @@ btnHold.addEventListener("click", function () {
       scores[activePlayer];
 
     // Check if the player score is == 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -79,3 +92,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", init);
